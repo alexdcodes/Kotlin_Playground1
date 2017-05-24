169a58e4daa6715c00940d028ea8242b8de2fc5a
Alex Diker <alex.diker@georgebrown.ca>
2017-05-24 03:05:27 -0400

    clean up
    clean up crew.

--- src/app.kt
+++ src/app.kt
@@ -15,20 +15,26 @@ fun main(args: Array<String>) {
     fun varargExample(vararg names: Int){
         println("Argument has ${names.size} elements")
     }
-    println("Marked with [vararg] Keyword to \nallow a variable number of arguments to be passed to a function\n")
+
+        println("Marked with [vararg] Keyword to \nallow a variable number of arguments to be passed to a function\n")
+
     varargExample()
     varargExample(1)
     varargExample(1,2,3) //=> has 3 elements
+    varargExample(1,2,3,4,5,500,200) //=> argument still has 7 elements
+
 
     fun welcome(name: String = "") : String {
         return "\nHello, $name!"
     }
 
-    println(welcome("Default User"))
+        println(welcome("Default User"))
 
-    println("\tDeclared Number: " + fooNum)
+        println("\tDeclared Number: " + fooNum)
 
     data class DataClassExample (val a: String, val x: Int, val y: Int, val z: Int)
+
     val fooData = DataClassExample("Default User", 1, 1991 , 4)
+    
     println(fooData) // => DataClassExample (a="Default User" x=1, y=1991, z=4)
 }
