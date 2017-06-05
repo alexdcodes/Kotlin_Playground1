0d8f3e5f5b4b02f3f4da5b49ce505f5cc6b046b8
Alex Diker <alex.diker@georgebrown.ca>
2017-06-05 02:20:04 -0400

    addded some extra
    added extra stuff

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-<h1>Kotlin Playground 1</h1>
+<h1>Kotlin Playground</h1>
 
 This is just a playground to learn some kotlin, nothing really special here.
+<br /><br />
+<i>just following a simple tutorial.</i>
+<br /><br />
+Will continue to add information here about the playground as time goes on.
--- src/app.kt
+++ src/app.kt
@@ -2,6 +2,7 @@
  * Created by Alex Diker on 5/24/2017.
  *
  * Just playing around with kotlin, this is just a playground
+ * Will continue to add information here as time goes on
  *
  */
 
@@ -23,7 +24,9 @@ fun main(args: Array<String>) {
     varargExample(1,2,3) //=> has 3 elements
     varargExample(1,2,3,4,5,500,200) //=> argument still has 7 elements
 
-
+    /*
+        Welcome function, hello kotlin
+    */
     fun welcome(name: String = "") : String {
         return "\nHello, $name!\n"
     }
@@ -31,7 +34,9 @@ fun main(args: Array<String>) {
         println(welcome("Default User"))
 
         println("\tDeclared Number: " + fooNum)
-
+    /*
+  Example of DataClass, adding String, and 3 Ints
+    */
     data class DataClassExample (val a: String, val x: Int, val y: Int, val z: Int)
 
     val fooData = DataClassExample("Default User", 1, 1984 , 10)
@@ -69,11 +74,21 @@ fun main(args: Array<String>) {
     /*
   Lets create a list using listOf function, this is immutable -- elements will not be added or removed
     */
-    println("\n")
+    println("\n" + "Car Company List:")
     val carList = listOf("BMW", "Audi", "Mercedes", "Tesla")
     println(carList.size)
     println(carList.first())
     println(carList.last())
     // Lets follow the tutorial and access the the list by index.
     println(carList[1] + "\n" + carList[2])
+
+    /*
+    Lets create a mutable list using mustableListOf
+    */
+    println("\n" + "Motorcycle Company List:")
+    val motorcycleMutableList = mutableListOf("Kawasaki", "Suzuki", "Honda")
+    motorcycleMutableList.add("Harley-Davidson")
+    motorcycleMutableList.add("Ducati")
+    println(motorcycleMutableList[1] + "\n" + motorcycleMutableList[2] + "\n" + motorcycleMutableList[3])
+    println(motorcycleMutableList[4])
 }
